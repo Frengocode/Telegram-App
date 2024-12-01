9c96b30c321372205f86c207d5e68582d98e03ba
Ibragim <164409199+Frengocode@users.noreply.github.com>
2024-12-01 20:39:44 +0500

    Update config.py

--- core/config.py
+++ core/config.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-PG_USERNAME = os.getenv("PG_USERNAME", "postgres")
-PG_PASSWORD = os.getenv("PG_PASSWORD", "python$_venv")
-PG_HOST = os.getenv("PG_HOST", "localhost:5432")
+PG_USERNAME = os.getenv("PG_USERNAME")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_HOST = os.getenv("PG_HOST")
